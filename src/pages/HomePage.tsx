1293a4e43d423ae9a3fcdb0bbde651a2281201bf
sivageetha-murugan <sivageetha.murugan@ideas2it.com>
2024-09-30 14:27:23 +0530

    Feat/ Added products in the home page

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,9 +1,13 @@
 import NavBar from '../components/NavBar'
+import Product from '../components/Product'
 
 function HomePage() {
   return (
     <>
       <NavBar />
+      <div className='p-8'>
+        <Product />
+      </div>
     </>
   )
 }
