a513562395918705be04fe1495d4a260c549dc43
sivageetha-murugan <sivageetha.murugan@ideas2it.com>
2024-09-30 12:32:38 +0530

    Feat/ Implemented NavBar in Home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
 import './App.css'
+import LoginPage from './pages/LoginPage'
+import HomePage from './pages/HomePage'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 function App() {
 
   return (
     <>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<LoginPage/>}/>
+          <Route path='/Home' element={<HomePage/>}/>
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
---
+++ src/components/NavBar.tsx
@@ -0,0 +1,40 @@
+import { Link } from 'react-router-dom'
+
+function NavBar() {
+  return (
+    <>
+      <nav className='h-20 w-full'>
+        <div className='flex'>
+          <div className='flex w-1/3 h-20 gap-x-6 justify-center items-center'>
+            <div>
+              <Link to="/Home">RINGS</Link>
+            </div>
+
+            <div><Link to="/Home">BRACELETS</Link></div>
+            <div><Link to="/Home">EARRINGS</Link></div>
+            <div><Link to="/Home">NECKLACES</Link></div>
+          </div>
+          <div className='flex w-1/3 h-20 items-center justify-center'>
+            <Link to="/Home">
+              <div className='flex flex-col items-center justify-center'>
+                <div>BLINGG</div>
+                <div>JEWELRY STORE</div>
+              </div>
+            </Link>
+          </div>
+          <div className='flex w-1/3 h-20 gap-x-6 justify-center items-center'>
+            <div><Link to="/Home">HOME</Link></div>
+            <div><Link to="/Home">ABOUT</Link></div>
+            <div><Link to="/Home">CONTACT</Link></div>
+            <div className='flex gap-x-6 justify-center items-center'>
+              <div><Link to="/Home"><i className="bi bi-search"></i></Link></div>
+              <div><Link to="/Home"><i className="bi bi-cart"></i></Link></div>
+            </div>
+          </div>
+        </div>
+      </nav>
+    </>
+  )
+}
+
+export default NavBar
